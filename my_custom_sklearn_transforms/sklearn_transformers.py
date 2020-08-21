8e84c45a32c0d077a607d63e8554d27d6fe71884
Santunis Monteiro de Oliveira <santunis.oliveira@fornecedores.sicoob.com.br>
2020-08-20 23:23:05 -0300

    ajuste ao remover colunas

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -17,30 +17,5 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
 # Instanciando uma transformação DropColumns\n",
 rm_columns = DropColumns(
-    columns=["NOTA_GO"]  # Essa transformação recebe como parâmetro uma lista com os nomes das colunas indesejadas
-)
-
-# Aplicando a transformação ``DropColumns`` ao conjunto de dados base
-rm_columns.fit(X=df_data_1)
-
-# Reconstruindo um DataFrame Pandas com o resultado da transformação
-df_data_2 = pd.DataFrame.from_records(
-    data=rm_columns.transform(
-        X=df_data_1
-    )
-)
-
-# Instanciando uma transformação DropColumns
-rm_columns = DropColumns(
-    columns=["INGLES"]  # Essa transformação recebe como parâmetro uma lista com os nomes das colunas indesejadas
-)
-
-# Aplicando a transformação ``DropColumns`` ao conjunto de dados base
-rm_columns.fit(X=df_data_2)
-
-# Reconstruindo um DataFrame Pandas com o resultado da transformação
-df_data_3 = pd.DataFrame.from_records(
-    data=rm_columns.transform(
-        X=df_data_2
-    )
+    columns=["NOTA_GO", "INGLES"]  # Essa transformação recebe como parâmetro uma lista com os nomes das colunas indesejadas
 )
